49baad9171a4f1c2dbacd7a4e012e15b74e4f01e
Abraham Sunday <sundayabraham934@gmail.com>
2022-03-10 13:54:23 -0800

    my c low level

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main- This is the main function
+ * Return: Value return should 0
+ */
+int main(void)
+{
+	char alx[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(alx);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main- This is the main function
+ * Return: The value must be 0
+ */
+int main(void)
+{
+	char alx[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf('%s", alx);
+	return (0);
+}
