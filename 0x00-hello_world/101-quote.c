c8ca6233c937e17abc204ffbdff27cb3233da4a1
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-10 23:09:02 +0100

    my c low level

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main- This is the main function
+ * Return: the value should be 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
