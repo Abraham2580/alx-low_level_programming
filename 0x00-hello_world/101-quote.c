7b9cb662584fa07a7534bd24c6633813d1ff74c7
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-11 10:05:45 +0100

    my c low level

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
- * main- This is the main function
- * Return: the value should be 1
+ * main - program's main functio
+ * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+int s;
+
+s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+s);
+return (1);
 }
