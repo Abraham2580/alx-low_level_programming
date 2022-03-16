dd88c1824692e8c6633b3e5ce8014bbbf89600f3
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:11:01 +0100

    fnl

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
-dudgubsdefsd
+#include "main.h"
+/**
+ * print_alphabet-This is the function to print all the alphabet
+ * Return: it shouldnt return any value
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
