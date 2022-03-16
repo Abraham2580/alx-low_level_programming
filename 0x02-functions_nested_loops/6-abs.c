f944ba1d05dfde7fea80f942342d4944e5701483
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:17:35 +0100

    fnl

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,15 @@
-bdvjdx bvkjb
+#include "main.h"
+/**
+ * _abs- to check the absolute value
+ * @n: n is an integer
+ * Return: integer
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
