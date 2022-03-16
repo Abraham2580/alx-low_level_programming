9cfcf5fdfd8e15be806fc4aa027f4a5bbcbaa1d4
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:13:28 +0100

    fnl

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,21 @@
-iugjdvbfbjdsvbfu
+#include "main.h"
+/**
+ * _isalpha- checks if a character is alphabert
+ * Return: returns 1 if true
+ * @c: ci= is an ascii character
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
