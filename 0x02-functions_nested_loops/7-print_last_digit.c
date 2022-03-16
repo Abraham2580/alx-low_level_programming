7b4b8cb96f4ce9a9878582831cfffc6930fc81f4
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:18:06 +0100

    fnl

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,18 @@
-jscfgsdcfyusvg
+#include "main.h"
+/**
+ * print_last_digit- prints last digit
+ * @n: n is an integer
+ * Return: integer
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (n < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
