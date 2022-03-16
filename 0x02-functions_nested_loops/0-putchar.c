99436446e34f417a28384fa7c729f38462cda458
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:10:39 +0100

    fnl

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
-dghd
+#include "main.h"
+/**
+ * main- This is the main function
+ * Return: it should return a value of 0
+ */
+int main(void)
+{
+
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
