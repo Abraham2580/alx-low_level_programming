ed8ea41b28e90defe9f9e26a218e132b8b42273a
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:12:13 +0100

    fnl

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
-kdsgubdfshfbg
+#include "main.h"
+/**
+ * print_alphabet_x10- it should print the alphabet 10 times
+ * Return: it shouldnt return anything
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int times = 0;
+
+	while (times < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		times++;
+	}
+}
