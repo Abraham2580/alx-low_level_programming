059d60805613d6412f92ca9e06b2a3fb352f0eaf
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:16:40 +0100

    fnl

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,24 @@
-jkshgfhsvfjbs
+#include "main.h"
+/**
+ * print_sign- tells what sign a number carries
+ * Return: returns + if carrying +
+ * @n: n is an integer
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
