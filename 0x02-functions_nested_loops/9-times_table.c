cd66aefe445ce7e49c6aed7a6892c5f4bfad47ce
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:19:27 +0100

    fnl

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,32 @@
-bjhctyvhbsdfvsdyb 
+#include "main.h"
+/**
+ * times_table- prints the times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int row, column, product, ones, tens;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
