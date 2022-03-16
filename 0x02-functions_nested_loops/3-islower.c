e37405dfa6d4f10574f08701811aa86cad31b74f
Abraham Sunday <99336381+Abraham2580@users.noreply.github.com>
2022-03-16 11:12:46 +0100

    fnl

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,17 @@
-dvgjhvs
+#include "main.h"
+/**
+ * _islower- This is to check if the alphabet is lower case
+ * Return: return 1 if lowercase
+ * @c: it is an ascii character
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
