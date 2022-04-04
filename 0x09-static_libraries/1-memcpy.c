3bd250fc4810b7dad1d6ba0a9daee75286c56e35
Abraham Sunday <sundayabraham934@gmail.com>
2022-04-04 01:13:37 +0100

    lib

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _memcpy - function thatcopies memory area
- * @dest: destination
- * @src: source
- * @n: number of bytes
- * Return: returns a pointer
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	*(dest + i) = *(src + i);
+		*(dest + i) =  *(src + i);
 
 	return (dest);
 }
